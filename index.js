2cf45be04cd69997d896da2ace12be69c291aef0
xdtinker <93765440+xdtinker@users.noreply.github.com>
2022-02-19 12:27:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { firefox } = require('playwright-firefox');
 const { send_log, send_notif } = require('./telegram.js');
-send_log("APP START");
+
 const args = [
     '--no-sandbox',
     '--disable-setuid-sandbox',
@@ -115,5 +115,6 @@ async function main() {
 }
 
 if (require.main === module) {
+    send_log("Apostille: APP Initializing");
     main();
 }
