583e05772cfc551284e23706d0c59cda994ab27a
xdtinker <93765440+xdtinker@users.noreply.github.com>
2022-03-24 11:51:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -94,8 +94,11 @@ async function main() {
                                 await page.reload()
                             }
                         } catch (e) {
-                            console.log('catch: Element missing, Reloading');
-                            await page.reload()
+                            if (error instanceof playwright.errors.TimeoutError)
+                                console.log('catch: Element missing, Reloading')
+                                await page.reload()
+  }
+                            
                             //await page.goto('https://co.dfaapostille.ph/appointment/Home/Index')
                         }
                     }
